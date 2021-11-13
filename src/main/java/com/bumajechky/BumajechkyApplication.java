eea6b94421600dc1f2ecd8176de5001517065f71
KorolkovOleg <helkorolkov@gmail.com>
2021-11-13 06:23:17 +0300

    initialise app

---
+++ src/main/java/com/bumajechky/BumajechkyApplication.java
@@ -0,0 +1,13 @@
+package com.bumajechky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BumajechkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BumajechkyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bumajechky/BumajechkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bumajechky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumajechkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
