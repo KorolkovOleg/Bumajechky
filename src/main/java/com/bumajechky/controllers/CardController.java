89e06ca148c5d49dc81f54d7deb8be4d320a65c9
KorolkovOleg <helkorolkov@gmail.com>
2021-11-30 05:40:41 +0300

    Добавлена возможность удаления и ссылка на шаблон card.

--- src/main/java/com/bumajechky/controllers/CardController.java
+++ src/main/java/com/bumajechky/controllers/CardController.java
@@ -5,10 +5,8 @@ import com.bumajechky.domain.Pack;
 import com.bumajechky.repositories.CardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/pack/{id}")
@@ -18,12 +16,23 @@ public class CardController {
     CardRepository cardRepository;
 
     @PostMapping("/createcard")
-    public String createCard(@ModelAttribute("card") Card card,
-                             @ModelAttribute("pack") Pack pack) {
+    public String createCard(   @ModelAttribute("card") Card card,
+                                @ModelAttribute("pack") Pack pack) {
         card.setPack(pack);
         cardRepository.save(card);
         return "redirect:/pack/" + pack.getId() + "/";
     }
 
+    @DeleteMapping("/deletecard/{cardId}")
+    public String deleteCard(   @ModelAttribute("pack") Pack pack,
+                                @PathVariable("cardId") Long cardId) {
+        cardRepository.deleteById(cardId);
+        return "redirect:/pack/" + pack.getId() + "/";
+    }
 
+    @GetMapping("/{cardId}")
+    public String getCard(@PathVariable("cardId") Long cardId, ModelMap modelMap) {
+        Card card = cardRepository.findById(cardId).get();
+        return "card";
+    }
 }
